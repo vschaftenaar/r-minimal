255dd4d2492df4b9c9c13a0729af4f520e9f7db2
vschaftenaar <4079204+vschaftenaar@users.noreply.github.com>
2024-12-08 14:50:29 +0100

    update

--- run.bat
+++ run.bat
@@ -23,42 +23,10 @@ set RScriptFolder=src
 rem Set the r-script (without .r extention)
 set RScriptName=main
 
-rem Create the subfolder if it does not exist
-if not exist %RScriptFolder% (
-mkdir %RScriptFolder%
-)
-
-
-
-rem Create the script file if it does not exist
-if not exist %RScriptFolder%\%RScriptName%.r (
-echo message('Could not find script, so it has been created'^) > %RScriptFolder%\%RScriptName%.r
-echo  Sys.sleep(5^) >> %RScriptFolder%\%RScriptName%.r
-)
-
 rem Set the base path of the R installation
-set RBasePath="C:\Program Files\R"
+set RScript64="%~dp0env\R-4.4.2\bin\x64\Rscript.exe"
 
-rem Find the latest version of R installed
-for /f "delims=" %%a in ('dir /b /ad-h %RBasePath%\R-*') do (
-set RLatest=%%a
-)
-
-rem Construct the path to the RScript.exe
-set RScript64=%RBasePath%\%RLatest%\bin\x64\RScript.exe
-
-rem Check if RScript.exe exists
-if exist %RScript64% (
 rem Run the R script using RScript.exe
 %RScript64% "%~dp0%RScriptFolder%\%RScriptName%.r" %~dp0%
-if %errorlevel% neq 0 (
- echo An error occurred. Pausing the script.
- pause
-)
-) else (
-rem Display error message if RScript.exe is not found
-echo Error: RScript.exe not found
-pause
-)
 
-rem pause
+REM pause
